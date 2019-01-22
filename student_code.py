80593df259fc5a46242b0222fb79c2b8c23fa72a
XijingLiu <xijingliu2020@u.northwestern.edu>
2019-01-22 14:11:58 -0600

    add assert and ask

--- student_code.py
+++ student_code.py
@@ -24,6 +24,21 @@ class KnowledgeBase(object):
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
         print("Asserting {!r}".format(fact))
+        for i in range(0, len(self.facts)):
+            if fact == self.facts[i]:
+                print("Already in KB")
+                return
+        for i in range(0, len(self.rules)):
+            if fact == self.rules[i]:
+                print("Already in KB")
+                return
+        if isinstance(fact, Fact):
+            self.facts.append(fact)
+        else:
+            self.rules.append(fact)
+
+
+
         
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
@@ -35,3 +50,23 @@ class KnowledgeBase(object):
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
         print("Asking {!r}".format(fact))
+        ListOfBindings = []
+        for i in range(0,len(self.facts)):
+            #print(len(self.facts))
+            #print(type(facts))
+            #print(type(fact))
+            answer = match(fact.statement, self.facts[i].statement)
+            #print(fact.statement.terms)
+            #print(self.facts[i].statement.terms)
+            #print(i)
+            if answer != False:
+               ListOfBindings.append(answer)
+        if len(ListOfBindings) == 0:
+            return False
+        return ListOfBindings
+
+
+
+
+
+
